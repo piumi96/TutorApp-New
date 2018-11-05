0a859a005baa3ffbd0246bde6e8a18c4b5b17cc1
piumi96 <piumidp96@gmail.com>
2018-11-06 01:12:25 +0530

    cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const passportSetup = require('./config/passport-setup');
 const app = express();
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://guarded-beyond-19031.herokuapp.com");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
