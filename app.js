c7c793bfc4cc3c1b4c8d63d79267c28d0921d86a
piumi96 <piumidp96@gmail.com>
2018-11-05 20:42:56 +0530

    cors

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ const passportSetup = require('./config/passport-setup');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(bodyParser.urlencoded({ extends: false }));
 app.use(bodyParser.json());
 
@@ -26,11 +32,7 @@ con.connect((err) => {
 });
 
 //app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
 
 /* app.use(function (req, res, next) {
 
