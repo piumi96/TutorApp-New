56f8311d3948464f0e6b145db62721266edb300b
piumi96 <piumidp96@gmail.com>
2018-11-05 20:42:17 +0530

    cors

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ con.connect((err) => {
 
 });
 
-app.use(cors());
+//app.use(cors());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
