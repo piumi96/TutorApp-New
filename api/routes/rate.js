251ce61e5c56c07be7302447cbf74d93ee9dee83
piumi96 <piumidp96@gmail.com>
2018-11-20 18:55:53 +0530

    rate route updated

--- api/routes/rate.js
+++ api/routes/rate.js
@@ -16,43 +16,62 @@ router.post('/rate', (req, res) => {
         }
     })
 
-    var sql = "insert into Rate(tutor, student, rating) values('"+tutor+"', '"+student+"', '"+rate+"')";
-    con.query(sql, (err, result) => {
-        if(err){
-            throw err;
-            res.json({
-                success: false,
-                rating: rating
-            });            
-        } 
-        else{
-            var sql1 = "update Tutor set Tutor.rate = (select avg(Rate.rating) as rate from Rate where Rate.tutor='"+tutor+"') where Tutor.email='"+tutor+"'";
-            con.query(sql1, (err, result) => {
+    var sql3 = "select * from Requests where tutor='"+tutor+"' and student='"+student+"' and status='ACCEPTED'";
+    con.query(sql3, (err, result) => {
+        if(err) throw err;
+        else if(result.length != 0){
+            console.log(result);
+            var sql = "insert into Rate(tutor, student, rating) values('"+tutor+"', '"+student+"', '"+rate+"')";
+            con.query(sql, (err, result) => {
                 if(err){
                     throw err;
                     res.json({
                         success: false,
-                        rating: rating
-                    });
-                }
+                        rating: rating,
+                        allowed: true
+                    });            
+                } 
                 else{
-                    var sql2 = "select rate from Tutor where Tutor.email='"+tutor+"'";
-                    con.query(sql2, (err, result) => {
-                    if(err) throw err;
-                    else{
-                        rating = result[0].rate;
+                    var sql1 = "update Tutor set Tutor.rate = (select avg(Rate.rating) as rate from Rate where Rate.tutor='"+tutor+"') where Tutor.email='"+tutor+"'";
+                    con.query(sql1, (err, result) => {
+                        if(err){
+                            throw err;
+                            res.json({
+                                success: false,
+                                rating: rating,
+                                allowed: true
+                            });
                         }
-                    });
-
-                    res.json({
-                        success: true,
-                        rating: rating
-                    });
+                        else{
+                            var sql2 = "select rate from Tutor where Tutor.email='"+tutor+"'";
+                            con.query(sql2, (err, result) => {
+                            if(err) throw err;
+                            else{
+                                rating = result[0].rate;
+                                }
+                            });
+        
+                            res.json({
+                                success: true,
+                                rating: rating,
+                                allowed: true
+                            });
+                        }
+                    })
+                    
                 }
+            });
+
+        }
+        else{
+            res.json({
+                success: false,
+                rating: rating,
+                allowed: false
             })
-            
         }
     });
+
 })
 
 module.exports = router;
