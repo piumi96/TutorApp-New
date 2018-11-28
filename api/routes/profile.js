dba979d209fb85c8f1908168309995e5efc7ec1e
piumi96 <piumidp96@gmail.com>
2018-11-28 23:31:50 +0530

    view profile student route added

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -4,52 +4,82 @@ const con = require('../../databse/db');
 
 router.get('/viewProfile', (req, res) => {
     var email = req.body.email;
-    var sql = "select FirstName, LastName, Location, Mobile, Subject, Rate, ImgURL from Tutor where email='"+email+"'";
-    var sql1 = "select * from Review where tutor='"+email+"'";
-    var profile;
-    var reviews = [];
+    var role = req.body.role;
 
-    con.query(sql, (err, result) => {
-        if(err) {
-            res.json({
-                profile: null,
-                reviews: null
-            }) ;        
-        }
-        else{
-            profile = {
-               email: email,
-               FirstName: result[0].FirstName,
-               LastName: result[0].LastName,
-               Location: result[0].Location,
-               Mobile: result[0].Mobile,
-               Subject: result[0].Subject,
-               Rate: result[0].Rate,
-               ImgUrl: result[0].ImgURL
-           }
+    if(role=='tutor'){
+        var sql = "select FirstName, LastName, Location, Mobile, Subject, Rate, ImgURL from Tutor where email='"+email+"'";
+        var sql1 = "select * from Review where tutor='"+email+"'";
+        var profile;
+        var reviews = [];
+    
+        con.query(sql, (err, result) => {
+            if(err) {
+                res.json({
+                    profile: null,
+                    reviews: null
+                }) ;        
+            }
+            else{
+                profile = {
+                   email: email,
+                   FirstName: result[0].FirstName,
+                   LastName: result[0].LastName,
+                   Location: result[0].Location,
+                   Mobile: result[0].Mobile,
+                   Subject: result[0].Subject,
+                   Rate: result[0].Rate,
+                   ImgUrl: result[0].ImgURL
+               }
+    
+               con.query(sql1, (err, response) => {
+                   if(err){
+                       console.log(err);
+                   }
+                   else{
+                       for(var i=response.length-1; i>=0; i--){
+                            reviews[i - (response.length - 1)] = {
+                               date: response[i].date,
+                               tutor: response[i].tutor,
+                               student: response[i].student,
+                               content: response[i].content, 
+                           }
+                       } 
+                       res.json({
+                           profile: profile,
+                           reviews: reviews
+                       });
+                   }
+               })
+               
+            }
+        })   
 
-           con.query(sql1, (err, response) => {
-               if(err){
-                   console.log(err);
-               }
-               else{
-                   for(var i=response.length-1; i>=0; i--){
-                        reviews[i - (response.length - 1)] = {
-                           date: response[i].date,
-                           tutor: response[i].tutor,
-                           student: response[i].student,
-                           content: response[i].content, 
-                       }
-                   } 
-                   res.json({
-                       profile: profile,
-                       reviews: reviews
-                   });
-               }
-           })
-           
-        }
-    })   
+    }
+    else if(role=='student'){
+        var sql = "select * from Student where email='"+email+"'";
+
+        con.query(sql, (err, result) => {
+            if(err){
+                console.log(err);
+                res.json({
+                    profile: null
+                });
+            }
+            else{
+                console.log(result);
+                var profile = {
+                    Name: result[0].name,
+                    Mobile: result[0].mobile,
+                    Location: result[0].location,
+                    email: email
+                }
+
+                res.json({
+                    profile: profile
+                });
+            }
+        })
+    }
     
 });
 
@@ -99,7 +129,6 @@ router.put('/editProfile', (req, res) => {
                 });
             }
             else {
-                //console.log(result);
                 res.send({
                     success: true
                 });
