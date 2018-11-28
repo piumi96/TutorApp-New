e903ce7866083fe814fe1ddd01ec4eb91e76aeb8
piumi96 <piumidp96@gmail.com>
2018-11-28 20:21:16 +0530

    make suggestions route added

--- api/routes/suggestions.js
+++ api/routes/suggestions.js
@@ -5,7 +5,7 @@ const con = require('../../databse/db');
 
 router.get('/viewAllSuggestions', (req, res) => {
     var sql = "select * from Suggestions";
-    var suggestions = [];
+    var suggestion = [];
 
     con.query(sql, (err, result) => {
         if(err){
@@ -24,7 +24,7 @@ router.get('/viewAllSuggestions', (req, res) => {
             }
 
             res.json({
-                suggestions: suggestions
+                suggestions: suggestion
             });
         }
     })
@@ -32,8 +32,9 @@ router.get('/viewAllSuggestions', (req, res) => {
 
 router.get('/mySuggestions', (req, res) => {
     var email = req.body.email;
-
+    var suggestion = [];
     var sql="select*from Suggestions where Sender='"+email+"'";
+
     con.query(sql, (err, result) => {
         if(err){
             res.json({
@@ -51,7 +52,28 @@ router.get('/mySuggestions', (req, res) => {
             }
 
             res.json({
-                suggestions: suggestions
+                suggestions: suggestion
+            });
+        }
+    })
+})
+
+router.post('/makeSuggestion', (req, res) => {
+    var email = req.body.email;
+    var content = req.body.content;
+
+    var sql = "insert into Suggestions(Sender, Date, Content) values('"+email+"', CURDATE(), '"+content+"')";
+
+    con.query(sql, (err, result) => {
+        if(err){
+            console.log(err);
+            res.json({
+                success: false
+            });
+        }
+        else{
+            res.json({
+                success: true
             });
         }
     })
