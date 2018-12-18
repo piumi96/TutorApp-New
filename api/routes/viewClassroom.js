0953adbe25878defd3bb1da3d1859521c59fe9a7
piumi96 <piumidp96@gmail.com>
2018-12-18 18:56:58 +0530

    databsae up

--- api/routes/viewClassroom.js
+++ api/routes/viewClassroom.js
@@ -1,49 +1,3 @@
-/* const express = require('express');
-const passport = require('passport');
-const request = require('request');
-const keys = require('../../config/keys');
-const user = require('../../config/passport-setup');
-const con = require('../../databse/db');
-
-const router = express.Router();
-
-router.get('/class', passport.authenticate('googleClass', { 
-    scope: ['email', 'https://www.googleapis.com/auth/classroom.courses']
-    
-}), (req, res) => {
-        req.session.scope = req.user.scope;
-        req.session.access_token = req.user.access_token;
-        req.session.refresh_token = req.user.refresh_token;
-        var scope = req.user.scope;
-        var accessToken = req.user.access_token;
-        var refreshToken = req.user.refresh_token;
-
-
-    })
-
-router.get('/class/courses', passport.authenticate('googleClass'), (req, res) => {
-    request('https://classroom.googleapis.com/v1/courses', function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred and handle it
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        res.send(body)
-    });
-})
-
-/* const Client = require('google-classroom');
-
-const client = new Client({
-    clientId: keys.googleClassroom.clientID,
-    clientSecret: keys.googleClassroom.clientSecret,
-    refreshToken: ''
-})
-
-client.on('ready', async classr => {
-    client.getCourses()
-        .then(data => {
-            console.log(data)
-        })
-}) */
-
 const express = require('express');
 const router = express.Router();
 const con = require('../../databse/db');
@@ -53,12 +7,25 @@ const readline = require('readline');
 const {google} = require('googleapis');
 
 router.get('/viewCourses', (req, res) => {
+
+    //var action = req.body.action;
+    action = 'viewCourse';
+
     const SCOPES = ['https://www.googleapis.com/auth/classroom.courses'];
     const TOKEN_PATH = 'token.json';
-    fs.readFile('credentials.json', (err, content) => {
-        if(err) return console.log('Error loading client secret file:', err);
-        authorize(JSON.parse(content), listCourses);
-    });
+
+    if (action === 'viewCourse') { 
+        fs.readFile('credentials.json', (err, content) => {
+            if(err) return console.log('Error loading client secret file:', err);
+            authorize(JSON.parse(content), listCourses);
+        });
+    }
+    else if(action==='createCourse'){
+        fs.readFile('credentials.json', (err, content) => {
+            if (err) return console.log('Error loading client secret file:', err);
+            authorize(JSON.parse(content), createCourses);
+        });
+    }
     
     function authorize(credentials, callback){
         const {client_secret, client_id, redirect_uris} = credentials.installed;
@@ -126,6 +93,16 @@ router.get('/viewCourses', (req, res) => {
         })
     }
 
+   /*  function createCourses(auth){
+        const classsroom = google.classroom({ version: 'v1', auth });
+        var newCourse = {
+            name: 'abc',
+            ownerId: 'me',
+            courseState: 'PROVISIONED'
+        }
+        classroom.courses.create(newCourse);
+    
+    } */
 })
 
 
---
+++ databse/database.sql
@@ -0,0 +1,68 @@
+create table District(
+		DistrictID int primary key auto_increment,
+		name varchar(30)
+);
+
+create table Tutor(
+		TutorID int primary key auto_increment,
+		email varchar(50),
+		password varchar(100),
+		FirstName varchar(50),
+		LastName varchar(50),
+		Location varchar(30),
+		Mobile varchar(10),
+		Subject varchar(30),
+		Rate float,
+		ImgURL varchar(200),
+		Price float,
+		Available_time date,
+		status boolean
+);
+
+create table Student(
+		StudentID int primary key auto_increment,
+		email varchar(50),
+		pword varchar(100),
+		name varchar(50),
+		mobile varchar(10),
+		location varchar(30),
+		status boolean
+)
+
+create table Rate(
+		RateID int primary key auto_increment,
+		tutor varchar(50),
+		student varchar(50),
+		rating float
+);
+
+create table Requests(
+		reqID int primary key auto_increment,
+		student varchar(50),
+		tutor varchar(50),
+		sent_date date,
+		day varchar(30),
+		location varchar(50),
+		subject varchar(50),
+		status varchar(50)
+);
+
+create table Review(
+		ReviewID int primary key auto_increment,
+		tutor varchar(50),
+		student varchar(50),
+		date date,
+		content varchar(500)
+);
+
+create table Subject(
+		SubjectID int primary key auto_increment,
+		Name varchar(50)
+);
+
+create table Suggestions(
+		SuggestionID int primary key auto_increment,
+		Sender varchar(50),
+		Date date,
+		Content varchar(500)
+);
