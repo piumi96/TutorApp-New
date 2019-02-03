509b240c9dcc28f17bac1c90345bd3a5d719503d
piumi96 <piumidp96@gmail.com>
2019-02-03 17:22:15 +0530

    classroom updates

--- api/routes/classroom.js
+++ api/routes/classroom.js
@@ -5,17 +5,14 @@ const keys = require('../../config/keys');
 const { google } = require('googleapis');
 const Client = require('google-classroom');
 
-const fs = require('fs');
-const readline = require('readline');
 const SCOPES = ['https://www.googleapis.com/auth/classroom.courses', 'https://www.googleapis.com/auth/classroom.rosters', 'https://www.googleapis.com/auth/classroom.coursework.me', 'https://www.googleapis.com/auth/classroom.coursework.students'];
-const TOKEN_PATH = 'googleToken.json';
+var refresh = null;
 
 const credentials = {
     client_secret: keys.oauthClient.clientSecret,
     client_id: keys.oauthClient.clientID,
     redirect_uris: ["urn:ietf:wg:oauth:2.0:oob", "http://localhost:3000/", "https://classroom.googleapis.com/v1/courses"]
 };
-
 
 function authorize(credentials) {
     const client_secret = credentials.client_secret;
@@ -33,56 +30,9 @@ function authorize(credentials) {
     });
 
     return authUrl;
-    /* fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oAuth2Client, callback);
-        oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client);
-    }); */
 }
 
-/* function getNewToken(oAuth2Client, callback) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        prompt: 'consent',
-        scope: SCOPES,
-    });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-
-    rl.question('Enter the code from that page here: ', (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err) return console.error('Error retireving access token', err);
-            oAuth2Client.setCredentials(token);
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) console.error(err);
-                console.log('Token stored to', TOKEN_PATH);
-            });
-            callback(oAuth2Client);
-
-        });
-
-    });
-}; */
-
-var refresh = null;
 router.post('/listCourses', (req, res) => {
-    /* fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err){
-            console.log(err);
-            res.json({
-                success:false
-            });
-        }
-        else{
-            refresh = JSON.parse(token).refresh_token;
-            console.log(refresh);
-        }
-    });
- */
     var code = req.body.token;
     if(code != "null"){
         const client_secret = credentials.client_secret;
@@ -129,89 +79,292 @@ router.post('/listCourses', (req, res) => {
             url: authUrl
         });
     }
-    /* authorize(credentials, listCourses);
-    function listCourses(){
-        const client = new Client({
-            clientId: keys.oauthClient.clientID,
-            clientSecret: keys.oauthClient.clientSecret,
-            refreshToken: refresh
-        })
-    
-        client.on('ready', async classr => {
-            client.getCourses()
-                .then(data => {
-                    res.json({
-                        courses: data
-                    });
-                });
-        });
-    } */
-
 });
 
 router.post('/createCourse', (req, res) => {
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) {
-            console.log(err);
-            res.json({
-                success: false
-            });
-        }
-        else {
-            refresh = JSON.parse(token).refresh_token;
-            console.log(refresh);
-        }
-    });
-    authorize(credentials, createCourses);
+    var code = req.body.token;
+    var section = req.body.section;
+    var name = req.body.name;
 
-    function createCourses() {
-        const client = new Client({
-            clientId: keys.oauthClient.clientID,
-            clientSecret: keys.oauthClient.clientSecret,
-            refreshToken: refresh
-        });
-        var section = req.body.section;
-        var name = req.body.name;
+    if (code != "null") {
+        const client_secret = credentials.client_secret;
+        const client_id = credentials.client_id;
+        const redirect_uris = credentials.redirect_uris;
 
-        client.on('ready', async classr => {
-            client.createCourse(name, section)
-                .then(data => {
-                    res.json({
-                        newCourse: data
-                    });
-                });
-        });
-    }
-});
+        const oAuth2Client = new google.auth.OAuth2(
+            client_id, client_secret, redirect_uris[0]
+        );
 
-
-router.get('/listInvites', (req, res) => {
-    authorize(credentials, listInvites);
-
-    function listInvites(auth) {
-        const classroom = google.classroom({ version: 'v1', auth });
-        classroom.invitations.list({
-            userId: 'me'
-        }, (err, response) => {
-            if(err){
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) {
                 console.log(err);
                 res.json({
                     success: false,
-                    invites: null
+                    msg: "error fetching access token"
                 });
             }
-            else{
-                console.log(response);
-                res.json({
-                    success: true,
-                    invites: response.data
+            else {
+                console.log(token);
+                refresh = token.refresh_token;
+
+                const client = new Client({
+                    clientId: keys.oauthClient.clientID,
+                    clientSecret: keys.oauthClient.clientSecret,
+                    refreshToken: refresh
                 })
+                
+                client.on('ready', async classr => {
+                    client.createCourse(name, section)
+                        .then(data => {
+                            res.json({
+                                newCourse: data
+                            });
+                        });
+                });
+                
             }
         })
     }
+    else {
+        var authUrl = authorize(credentials);
+        console.log(authUrl);
+        res.json({
+            url: authUrl
+        });
+    }
 });
 
-router.post('/acceptInvite', (req, res) => {
+router.post('/listInvites', (req, res) => {
+    var code = req.body.token;
+    if (code != "null") {
+        const client_secret = credentials.client_secret;
+        const client_id = credentials.client_id;
+        const redirect_uris = credentials.redirect_uris;
+
+        const oAuth2Client = new google.auth.OAuth2(
+            client_id, client_secret, redirect_uris[0]
+        );
+
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) {
+                console.log(err);
+                res.json({
+                    success: false,
+                    msg: "error fetching access token"
+                });
+            }
+            else {
+                console.log(token);
+                refresh = token.refresh_token;
+                var access = token.access_token;
+
+                const classroom = google.classroom({ version: 'v1', oAuth2Client});
+                classroom.invitations.list({
+                    userId: 'me'
+                }, (err, response) => {
+                    if (err) {
+                        console.log(err);
+                        res.json({
+                            success: false,
+                            invites: null
+                        });
+                    }
+                    else {
+                        console.log(response);
+                        res.json({
+                            success: true,
+                            invites: response.data
+                        })
+                    }
+                })
+
+            }
+        })
+    }
+    else{
+        var authUrl = authorize(credentials);
+        console.log(authUrl);
+        res.json({
+            url: authUrl
+        });
+    }
+});
+
+//teachers of a course
+router.post('/listTeachers', (req, res) => {
+    var code = req.body.token;
+    var courseId = req.body.courseId;
+
+    if (code != "null") {
+        const client_secret = credentials.client_secret;
+        const client_id = credentials.client_id;
+        const redirect_uris = credentials.redirect_uris;
+
+        const oAuth2Client = new google.auth.OAuth2(
+            client_id, client_secret, redirect_uris[0]
+        );
+
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) {
+                console.log(err);
+                res.json({
+                    success: false,
+                    msg: "error fetching access token"
+                });
+            }
+            else {
+                console.log(token);
+                refresh = token.refresh_token;
+
+                const classroom = google.classroom({ version: 'v1', oAuth2Client });
+                classroom.courses.teachers.list({
+                    courseId: courseId
+                }, (err, response) => {
+                    if (err) {
+                        console.log(err);
+                        res.json({
+                            success: false,
+                            teachers: null
+                        })
+                    }
+                    else {
+                        console.log(response.data.teachers[0].profile);
+                        res.json({
+                            success: true,
+                            teachers: response.data.teachers
+                        })
+                    }
+                })
+
+            }
+        })
+    }
+    else {
+        var authUrl = authorize(credentials);
+        console.log(authUrl);
+        res.json({
+            url: authUrl
+        });
+    }
+});
+
+//students of a course
+router.post('/listStudents', (req, res) => {
+    var code = req.body.token;
+    var courseId = req.body.courseId;
+
+    if (code != "null") {
+        const client_secret = credentials.client_secret;
+        const client_id = credentials.client_id;
+        const redirect_uris = credentials.redirect_uris;
+
+        const oAuth2Client = new google.auth.OAuth2(
+            client_id, client_secret, redirect_uris[0]
+        );
+
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) {
+                console.log(err);
+                res.json({
+                    success: false,
+                    msg: "error fetching access token"
+                });
+            }
+            else {
+                console.log(token);
+                refresh = token.refresh_token;
+
+                const classroom = google.classroom({ version: 'v1', oAuth2Client });
+                classroom.courses.students.list({
+                    courseId: courseId
+                }, (err, response) => {
+                    if (err) {
+                        console.log(err);
+                        res.json({
+                            success: false,
+                            students: null
+                        })
+                    }
+                    else {
+                        console.log(response);
+                        res.json({
+                            success: true,
+                            students: response.data.students
+                        })
+                    }
+                })
+
+            }
+        })
+    }
+    else {
+        var authUrl = authorize(credentials);
+        console.log(authUrl);
+        res.json({
+            url: authUrl
+        });
+    }
+});
+
+//courseWork
+router.post('/listCourseWork', (req, res) => {
+    var code = req.body.token;
+    var courseId = req.body.courseId;
+
+    if (code != "null") {
+        const client_secret = credentials.client_secret;
+        const client_id = credentials.client_id;
+        const redirect_uris = credentials.redirect_uris;
+
+        const oAuth2Client = new google.auth.OAuth2(
+            client_id, client_secret, redirect_uris[0]
+        );
+
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) {
+                console.log(err);
+                res.json({
+                    success: false,
+                    msg: "error fetching access token"
+                });
+            }
+            else {
+                console.log(token);
+                refresh = token.refresh_token;
+
+                const classroom = google.classroom({ version: 'v1', oAuth2Client });
+                classroom.courses.courseWork.list({
+                    courseId: req.body.courseId,
+                }, (err, response) => {
+                    if (err) {
+                        console.log(err);
+                        res.json({
+                            success: false,
+                            courseWork: null
+                        })
+                    }
+                    else {
+                        console.log(response.data);
+                        res.json({
+                            success: true,
+                            courseWork: response.data
+                        });
+                    }
+                })
+
+            }
+        })
+    }
+    else {
+        var authUrl = authorize(credentials);
+        console.log(authUrl);
+        res.json({
+            url: authUrl
+        });
+    }
+});
+
+/* router.post('/acceptInvite', (req, res) => {
     fs.readFile(TOKEN_PATH, (err, token) => {
         if (err) {
             console.log(err);
@@ -241,10 +394,10 @@ router.post('/acceptInvite', (req, res) => {
                 });
         });
     }
-});
+}); */
 
 //invalid json payload???
-router.post('/sendInvite', (req, res) => {
+/* router.post('/sendInvite', (req, res) => {
     authorize(credentials, sendInvite);
 
     function sendInvite(auth){
@@ -265,89 +418,7 @@ router.post('/sendInvite', (req, res) => {
             }
         })
     }
-})
-
-//teachers of a course
-router.get('/listTeachers', (req, res) => {
-    authorize(credentials, listTeachers);
-
-    function listTeachers(auth){
-        const classroom = google.classroom({ version: 'v1', auth });
-        classroom.courses.teachers.list({
-            courseId: req.body.courseId
-        }, (err, response) => {
-            if(err){
-                console.log(err);
-                res.json({
-                    success: false,
-                    teachers: null
-                })
-            }
-            else{
-                console.log(response.data.teachers[0].profile);
-                res.json({
-                    success: true,
-                    teachers: response.data.teachers
-                })
-            }
-        })
-    }
-});
-
-//students of a course
-router.get('/listStudents', (req, res) => {
-    authorize(credentials, listStudents);
-
-    function listStudents(auth) {
-        const classroom = google.classroom({ version: 'v1', auth });
-        classroom.courses.students.list({
-            courseId: req.body.courseId
-        }, (err, response) => {
-            if (err) {
-                console.log(err);
-                res.json({
-                    success: false,
-                    students: null
-                })
-            }
-            else {
-                console.log(response);
-                res.json({
-                    success: true,
-                    students: response.data.students
-                })
-            }
-        })
-    }
-});
-
-//courseWork
-router.get('/listCourseWork', (req, res) => {
-    authorize(credentials, listCourseWork);
-
-    function listCourseWork(auth){
-        const classroom = google.classroom({ version: 'v1', auth });
-        classroom.courses.courseWork.list({
-            courseId: req.body.courseId,          
-        }, (err, response) => {
-            if(err){
-                console.log(err);
-                res.json({
-                    success: false,
-                    courseWork: null
-                })
-            }
-            else{
-                console.log(response.data);
-                res.json({
-                    success: true,
-                    courseWork: response.data
-                });
-            }
-        })
-    }
-});
-
+}) */
 
 
 module.exports = router;
