b695ae26bc1c72b9240753a95686c3cfe8c38265
piumi96 <piumidp96@gmail.com>
2019-02-04 16:59:43 +0530

    graph route updated

--- api/routes/tutorDash.js
+++ api/routes/tutorDash.js
@@ -36,6 +36,38 @@ router.get('/tutorDash', (req, res) => {
             });
         }
     })
+});
+
+router.post('/graph', (req, res) => {
+    var tutor = req.body.tutor;
+    var subject = req.body.subject;
+    var count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    var sql = "select email, subject, DistrictID from Tutor, District where Subject='"+subject+"' and Tutor.Location = District.name";
+    con.query(sql, (err, result) => {
+        if(err){
+            console.log(err);
+            res.json({
+                success: false,
+                count: null
+            });
+        }
+        else{
+            console.log(result);
+            
+            for(var i=0; i<result.length; i++){
+                for(var j=1; j<26; j++){
+                    if(result[i].DistrictID==j){
+                        count[j-1]++;
+                    }
+                }
+            }
+            res.json({
+                success: true,
+                count: count
+            });
+        }
+    })
 })
 
 module.exports = router;
